88e415bd4c319cf505caa2eb211a1f2bdcc1ba07
zhaoxiaoer <445774833@qq.com>
2018-08-03 10:08:56 +0800

    add servlet template

---
+++ src/c3/ServletTemplate.java
@@ -0,0 +1,24 @@
+package c3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletTemplate extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException{
+		
+		// Use "request" to read incoming HTTP headers
+		// (e.g., cookies) and query data from HTML forms.
+		
+		// Use "response" to specify the HTTP response status
+		// code and headers (e.g., the content type, cookies).
+		
+		PrintWriter out = res.getWriter();
+		// Use "out" to send content to browser
+	}
+}
